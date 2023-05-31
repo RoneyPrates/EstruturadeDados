aa87f28b65b9407467606a25ff6492b20a663d59
Roney <107938728+RoneyPrates@users.noreply.github.com>
2023-05-30 21:55:45 -0300

    Add files via upload

---
+++ exempoMenu/src/main/java/com/mycompany/exempomenu/ExempoMenu.java
@@ -0,0 +1,54 @@
+
+package com.mycompany.exempomenu;
+
+import javax.swing.JOptionPane;
+
+
+/**
+ *
+ * @author aluno
+ */
+public class ExempoMenu {
+
+    public static void main(String[] args) {
+        
+        int opcao = -1 ;
+        
+        while(opcao != 0){ 
+        opcao = Integer.parseInt(JOptionPane.showInputDialog(
+                "1 - Primeira opção \n"
+                + "2 - Segunda Opção \n"
+                + "3 - Terceira opção \n"
+                + "0 - Sair"));
+        
+            switch (opcao) {
+                case 1:
+                    primeirafuncao();
+            // executa a primeira função
+
+                    break;
+                case 2:
+                    segundaFuncao();
+            // executa a segunda função
+                   
+                    break;
+                case 3:
+                    terceiraFuncao();
+            //executa a terceira opção       
+                    
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+    public static void primeirafuncao(){
+    JOptionPane.showMessageDialog(null, "Entrou na primeira opção");
+    }
+    public static void segundaFuncao(){
+    JOptionPane.showMessageDialog(null, "Entrou na segunda opção");
+    }
+    public static void terceiraFuncao(){
+    JOptionPane.showMessageDialog(null, "Entrou na terceira opção");
+    }
+}
